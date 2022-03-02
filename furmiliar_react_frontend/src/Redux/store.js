0ac3b815194366a15131485ccadbc64af9e29716
dotnotation <dotinmotionproductions@gmail.com>
2022-03-01 21:33:22 -0500

    setting up store and basic reducer

---
+++ furmiliar_react_frontend/src/Redux/store.js
@@ -0,0 +1,5 @@
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers/reducer'
+
+export default createStore(reducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
