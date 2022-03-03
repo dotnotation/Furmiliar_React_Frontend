d8b5e884e3bfa151fbee2ee69e09229160a53af4
dotnotation <dotinmotionproductions@gmail.com>
2022-03-03 15:10:05 -0500

    added base Welcome page

--- furmiliar_react_frontend/src/App.js
+++ furmiliar_react_frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import PetsList from './Components/Pets/PetsList'
+import Welcome from './Containers/Welcome'
 
 function App() {
   return (
     <div>
       <h1>Furmiliar</h1>
       <Routes>
+        <Route path="/" element={<Welcome />} />
         <Route path="/pets" element={<PetsList />} />
       </Routes>
     </div>
