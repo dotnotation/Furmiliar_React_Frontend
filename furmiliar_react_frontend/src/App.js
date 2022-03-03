363dfe132ae2926b886b97ac8b4e6a6bb14b5ec0
dotnotation <dotinmotionproductions@gmail.com>
2022-03-03 15:12:20 -0500

    added very basic links for temporary navigation

--- furmiliar_react_frontend/src/App.js
+++ furmiliar_react_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import PetsList from './Components/Pets/PetsList'
 import Welcome from './Containers/Welcome'
@@ -7,6 +7,8 @@ function App() {
   return (
     <div>
       <h1>Furmiliar</h1>
+      <Link to="/">Home</Link><br></br>
+      <Link to="/pets">Pets</Link>
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/pets" element={<PetsList />} />
