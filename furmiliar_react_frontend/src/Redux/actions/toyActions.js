7f6a8e7c2db730b8f68a18938ce18accb589a2d5
dotnotation <dotinmotionproductions@gmail.com>
2022-03-03 16:47:02 -0500

    importing constants for actions

---
+++ furmiliar_react_frontend/src/Redux/actions/toyActions.js
@@ -0,0 +1,1 @@
+import { SET_TOYS, ADD_TOY, REMOVE_TOY, EDIT_TOY } from "./actionTypes"
--- furmiliar_react_frontend/src/Redux/reducers/toysReducers.js
+++ furmiliar_react_frontend/src/Redux/reducers/toysReducers.js
@@ -1,3 +1,5 @@
+import { SET_TOYS, EDIT_TOY, REMOVE_TOY, ADD_TOY } from "../actions/actionTypes"
+
 export default function toysReducers(state = {pets: [], toys: [], loading: false}, action){
     switch(action.type) {
         default:
