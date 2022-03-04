fc4e87a8e998a40db141ce494c023f7add23ce8e
dotnotation <dotinmotionproductions@gmail.com>
2022-03-04 13:07:44 -0500

    basic new pet form inputs and button

--- furmiliar_react_frontend/src/Components/Pets/PetForm.js
+++ furmiliar_react_frontend/src/Components/Pets/PetForm.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function PetForm() {
   return (
-    <div>PetForm</div>
+    <form className='toy-box-form'>
+        <label htmlFor='pet-name-input'>Pet's Name:</label>
+        <input type='text' id='pet-name-input' />
+        <label htmlFor='pet-photo-url-input'>URL of Pet's Photo:</label>
+        <input type='text' id='pet-photo-url-input' />
+        <button type='submit'>Register Pet</button>
+    </form>
   )
 }
