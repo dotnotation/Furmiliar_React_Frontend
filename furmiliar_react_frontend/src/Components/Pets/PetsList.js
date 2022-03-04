cfa7cb3887f7939bc7477a3178c0a2ad1ea8c488
dotnotation <dotinmotionproductions@gmail.com>
2022-03-04 16:16:15 -0500

    debugging petList sometimes loaded but other times not

--- furmiliar_react_frontend/src/Components/Pets/PetsList.js
+++ furmiliar_react_frontend/src/Components/Pets/PetsList.js
@@ -6,6 +6,10 @@ import PetCard from './PetCard'
 export default function PetsList() {
     const pets = useSelector(state => state.pets)
     const dispatch = useDispatch()
+    // console.log(pets)
+    // console.log(dispatch(fetchPets()))
+    // console.log(useEffect())
+    debugger
 
     useEffect(() => {
         dispatch(fetchPets())
@@ -13,9 +17,9 @@ export default function PetsList() {
     // pass in a callback and then a second argument of a dependency array
     // if we are using it as a componentDidMount, the dependency array will be empty
     // in the callback you want to call dispatch and the action creator
+
     return (
     <div className='pets-list'>
-        {console.log(pets)}
         {pets.map(p => <PetCard id={p.id} name={p.name} photo={p.photo}/>)}
     </div>
     )
