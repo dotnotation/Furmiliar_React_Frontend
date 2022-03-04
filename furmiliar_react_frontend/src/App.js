f158dcfde82a6b3926e293c951814bcfbadc0e6b
dotnotation <dotinmotionproductions@gmail.com>
2022-03-04 12:37:26 -0500

    base new pet form

--- furmiliar_react_frontend/src/App.js
+++ furmiliar_react_frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import PetForm from './Components/Pets/PetForm'
 import PetsList from './Components/Pets/PetsList'
 import Welcome from './Containers/Welcome'
 
@@ -8,10 +9,13 @@ function App() {
     <div>
       <h1>Furmiliar</h1>
       <Link to="/">Home</Link><br></br>
-      <Link to="/pets">Pets</Link>
+      <Link to="/pets">Pets</Link><br></br>
+      <Link to="/pets/new">Register Pet</Link><br></br>
+
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/pets" element={<PetsList />} />
+        <Route path="/pets/new" element={<PetForm />} />
       </Routes>
     </div>
   )
