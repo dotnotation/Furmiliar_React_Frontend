9bf4a4b919264eae5574a6dd318b876de824ced5
dotnotation <dotinmotionproductions@gmail.com>
2022-03-04 16:15:42 -0500

    working on onChange for pet form

--- furmiliar_react_frontend/src/Components/Pets/PetForm.js
+++ furmiliar_react_frontend/src/Components/Pets/PetForm.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function PetForm() {
+    const [name, setName] = useState('')
+    const [photo, setPhoto] = useState('')
+
   return (
     <form className='toy-box-form'>
         <label htmlFor='pet-name-input'>Pet's Name:</label>
-        <input type='text' id='pet-name-input' />
+        <input type='text' id='pet-name-input' value={name} onChange={e => setName(e.target.value)}/>
         <label htmlFor='pet-photo-url-input'>URL of Pet's Photo:</label>
-        <input type='text' id='pet-photo-url-input' />
+        <input type='text' id='pet-photo-url-input' value={photo} onChange={e => setPhoto(e.target.value)}/>
         <button type='submit'>Register Pet</button>
     </form>
   )
-}
+} 
