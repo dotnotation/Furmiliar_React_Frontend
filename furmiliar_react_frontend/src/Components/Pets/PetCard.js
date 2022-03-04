b50a994414cdf90dc32a893767efb457b9034c08
dotnotation <dotinmotionproductions@gmail.com>
2022-03-03 19:05:20 -0500

    abstracted logic for each PetCard out of PetsList

---
+++ furmiliar_react_frontend/src/Components/Pets/PetCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PetCard(props) {
+    console.log(props)
+  return (
+    <div className='pet-card' key={props.id}>
+        <h2>{props.name}</h2>
+        <img className="pet-photo" src={props.photo} alt={props.name}></img>
+    </div>
+  )
+}
--- furmiliar_react_frontend/src/Components/Pets/PetsList.js
+++ furmiliar_react_frontend/src/Components/Pets/PetsList.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchPets } from '../../Redux/actions/petActions'
+import PetCard from './PetCard'
 
 export default function PetsList() {
     const pets = useSelector(state => state.pets)
-    console.log(pets)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -13,16 +13,18 @@ export default function PetsList() {
     // pass in a callback and then a second argument of a dependency array
     // if we are using it as a componentDidMount, the dependency array will be empty
     // in the callback you want to call dispatch and the action creator
-    console.log(pets)
     return (
-    <div className='pet-cards'>
-        <h1>Pets</h1>
-
-        {pets.map(p => 
+    <div className='pets-list'>
+        {console.log(pets)}
+        {/* {pets.map(p => {
+            return (
             <span className='pet-card' key={p.id}>
                 <h2>{p.name}</h2>
-                <img className="pet-photo" src={p.photo}></img>
-            </span>)}
+                <img className="pet-photo" src={p.photo} alt={p.name}></img>
+            </span>)
+        })} */}
+        {pets.map(p => <PetCard id={p.id} name={p.name} photo={p.photo}/>)}
+        {/* {pets.map(p => <li key={p.id}>{p.name}</li>)} */}
     </div>
     )
 }
