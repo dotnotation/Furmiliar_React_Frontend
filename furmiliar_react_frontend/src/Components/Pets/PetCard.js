8f6c54964afbb109d564c9bfd9be10236b0783e1
dotnotation <dotinmotionproductions@gmail.com>
2022-03-07 13:42:29 -0500

    added edit and delete buttons to pet cards

--- furmiliar_react_frontend/src/Components/Pets/PetCard.js
+++ furmiliar_react_frontend/src/Components/Pets/PetCard.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function PetCard(props) {
     console.log(props)
@@ -6,6 +9,8 @@ export default function PetCard(props) {
     <div className='pet-card' key={props.id}>
         <h2>{props.name}</h2>
         <img className='pet-photo' src={props.photo} alt={props.name}></img>
+        <EditIcon id='pet-edit-button' data-action='edit' name='edit'/>
+        <DeleteIcon id='pet-delete-button' data-action='delete' name='delete' />
     </div>
   )
 }
