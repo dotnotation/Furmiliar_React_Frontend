ef9fa65f0db379b9d610df87f1a5d3cffcb861d3
dotnotation <dotinmotionproductions@gmail.com>
2022-03-07 15:24:46 -0500

    setting up basic onClick for edit and delete pet and made sure correct information is sent

--- furmiliar_react_frontend/src/Components/Pets/PetCard.js
+++ furmiliar_react_frontend/src/Components/Pets/PetCard.js
@@ -7,12 +7,20 @@ import { editPet, rainbowBridge } from '../../Redux/actions/petActions'
 export default function PetCard(props) {
     console.log(props)
     const dispatch = useDispatch()
+
+    function handleEdit(){
+      console.log("hit edit", props.id)
+    }
+
+    function handleDelete(){
+      console.log("hit delete", props.id)
+    }
   return (
     <div className='pet-card' key={props.id}>
         <h2>{props.name}</h2>
         <img className='pet-photo' src={props.photo} alt={props.name}></img>
-        <EditIcon id='pet-edit-button' data-action='edit' aria-labelledby='edit-button-label' role='button'/>
-        <DeleteIcon id='pet-delete-button' data-action='delete'  aria-labelledby='delete-button-label' role='button'/>
+        <EditIcon id='pet-edit-button' data-action='edit' aria-labelledby='edit-button-label' role='button' onClick={handleEdit}/>
+        <DeleteIcon id='pet-delete-button' data-action='delete'  aria-labelledby='delete-button-label' role='button' onClick={handleDelete}/>
     </div>
   )
 }
