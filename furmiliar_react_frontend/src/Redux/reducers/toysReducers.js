c9af590d82573cb78f9f540aee5809747c7633cd
dotnotation <dotinmotionproductions@gmail.com>
2022-03-08 13:08:10 -0500

    setting up toy reducers

--- furmiliar_react_frontend/src/Redux/reducers/toysReducers.js
+++ furmiliar_react_frontend/src/Redux/reducers/toysReducers.js
@@ -8,16 +8,35 @@ const initialState = {
 export default function toysReducers(state = initialState, action){
     switch(action.type) {
         case SET_TOYS:
-            return state
+            return {
+                ...state,
+                toys: action.payload
+            }
 
         case EDIT_TOY:
-            return state
+            const toyIndex = state.toys.findIndex(toy => toy.id === action.payload.id)
+            return {
+                ...state,
+                toys: [
+                    ...state.toys.slice(0, toyIndex),
+                    action.payload,
+                    ...state.toys.slice(toyIndex + 1)
+                ]
+            }
 
         case REMOVE_TOY:
-            return state
+            console.log("toysReducer", action.payload)
+            const newToys = state.toys.filter(toy => toy.id !== action.payload)
+            return {
+                ...state,
+                toys: newToys
+            }
 
         case ADD_TOY:
-            return state
+            return {
+                ...state, 
+                pets: [...state.pets, action.payload]
+            }
 
         default:
             return state
