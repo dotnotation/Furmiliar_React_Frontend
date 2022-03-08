6b463dfed7a2a59b393b19d09256a4db7019ba58
dotnotation <dotinmotionproductions@gmail.com>
2022-03-08 13:04:03 -0500

    setting up toy actions

--- furmiliar_react_frontend/src/Redux/actions/toyActions.js
+++ furmiliar_react_frontend/src/Redux/actions/toyActions.js
@@ -1,1 +1,58 @@
 import { SET_TOYS, ADD_TOY, REMOVE_TOY, EDIT_TOY } from './actionTypes'
+
+export function fetchToys(){
+    return dispatch => {
+        fetch("http://localhost:3000/toys")
+        .then(r => r.json())
+        .then(toys => dispatch({ type: SET_TOYS, payload: toys }))
+    }
+}
+
+export function adoptToy(toy){
+    return dispatch => {
+        fetch("http://localhost:3000/toys", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(toy)
+        })
+        .then(r => r.json())
+        .then(toy => {
+            dispatch({ type: ADD_TOY, payload: toy })
+        })
+    }
+}
+
+export function editToy(toy){
+    return dispatch => {
+        fetch(`http://localhost:3000/toys/${toy.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(toy)
+        })
+        .then(r => r.json())
+        .then(toy => {
+            dispatch({ type: EDIT_TOY, payload: toy })
+        })
+    }
+}
+
+export function deleteToy(id){
+    return dispatch => {
+        console.log(id)
+        fetch(`http://localhost:3000/toys/${id}`, {
+            method: 'DELETE'
+        })
+        .then(r => r.json())
+        .then(toy => {
+            console.log("second then", toy)
+            dispatch({ type: REMOVE_TOY, payload: toy })
+        })
+        // .catch(err => alert(err))
+    }
+}
