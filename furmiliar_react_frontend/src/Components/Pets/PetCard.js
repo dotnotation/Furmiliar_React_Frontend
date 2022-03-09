a367eb0b3d65f1d5799aa8a45fb0ab762e490e8b
dotnotation <dotinmotionproductions@gmail.com>
2022-03-09 16:50:54 -0500

    passing id from pet to toy list for conditional render which still needs to be set up

--- furmiliar_react_frontend/src/Components/Pets/PetCard.js
+++ furmiliar_react_frontend/src/Components/Pets/PetCard.js
@@ -3,10 +3,12 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import { useDispatch } from 'react-redux'
 import { editPet, rainbowBridge } from '../../Redux/actions/petActions'
+import { useNavigate } from 'react-router'
 
 export default function PetCard(props) {
     // console.log(props)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     function handleEdit(){
       console.log("hit edit", props.id)
@@ -20,10 +22,16 @@ export default function PetCard(props) {
     //   console.log("handleDelete", props)
     // }
 
+    function openToyBox(){
+      console.log("opening toy box", props.id)
+      navigate('/toys', {state: { pet_id: props.id}})
+    }
+
   return (
     <div className='pet-card' key={props.id}>
         <h2>{props.name}</h2>
         <img className='pet-photo' src={props.photo} alt={props.name}></img>
+        <button id='open-toy-box-button' data-action='open' aria-labelledby='open-toy-box-label' role='button' onClick={openToyBox}>Open Toy Box</button>
         <EditIcon id='pet-edit-button' data-action='edit' aria-labelledby='edit-button-label' role='button' onClick={handleEdit}/>
         <DeleteIcon id='pet-delete-button' data-action='delete'  aria-labelledby='delete-button-label' role='button' onClick={() => dispatch(rainbowBridge(props.id))}/>
     </div>
--- furmiliar_react_frontend/src/Components/Toys/ToysList.js
+++ furmiliar_react_frontend/src/Components/Toys/ToysList.js
@@ -3,15 +3,23 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchToys } from '../../Redux/actions/toyActions'
 import ToyCard from './ToyCard'
 import ToyForm from './ToyForm'
+import { useLocation } from 'react-router-dom'
 
 export default function ToysList() {
     const toys = useSelector(state => state.toysReduce.toys)
     const dispatch = useDispatch()
-
+    const location = useLocation()
+    const pet = location.state.pet_id
+    // console.log("props from navigate", props.id)
 
     useEffect(() => {
         dispatch(fetchToys())
     }, [])
+
+    useEffect(() => {
+      const pet = location.state
+    }, [location])
+    console.log(pet)
 
   return (
     <div className='toys-list'>
