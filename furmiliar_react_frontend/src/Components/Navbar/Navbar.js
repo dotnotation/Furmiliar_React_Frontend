124c15695d77ccbf0d0d212abc6362f735b20303
dotnotation <dotinmotionproductions@gmail.com>
2022-03-09 12:35:29 -0500

    navbar styling

--- furmiliar_react_frontend/src/Components/Navbar/Navbar.js
+++ furmiliar_react_frontend/src/Components/Navbar/Navbar.js
@@ -8,10 +8,11 @@ export default function NavigationBar() {
         <Navbar className='nav-bar' bg="light" expand="lg" >
             <Container fluid>
                 <Navbar.Brand href="/">Furmiliar</Navbar.Brand>
-                <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}>
-                <Nav.Link href="/toys">Toys</Nav.Link>
-                <Nav.Link href="/pets">Pets</Nav.Link>
-                <Nav.Link href="/pets/new">Register</Nav.Link>
+                {/* <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}> */}
+                <Nav className="me-auto mb-2 mb-lg-0">
+                <Nav.Link className="active fs-4" href="/toys">Toys</Nav.Link>
+                <Nav.Link className="active fs-4" href="/pets">Pets</Nav.Link>
+                <Nav.Link className="active fs-4" href="/pets/new">Register</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
