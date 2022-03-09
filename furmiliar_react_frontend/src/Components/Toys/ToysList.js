e0b7e293fd724cfb339a70684e79f144ec5c7cd5
dotnotation <dotinmotionproductions@gmail.com>
2022-03-09 18:28:17 -0500

    set up conditional toys to be filtered and rendered only if they match the pet

--- furmiliar_react_frontend/src/Components/Toys/ToysList.js
+++ furmiliar_react_frontend/src/Components/Toys/ToysList.js
@@ -6,23 +6,34 @@ import ToyForm from './ToyForm'
 import { useLocation } from 'react-router-dom'
 
 export default function ToysList() {
-    const toys = useSelector(state => state.toysReduce.toys)
-    const dispatch = useDispatch()
-    const location = useLocation()
-    const petId = location.state.pet_id
-    const petName = location.state.name
+  const toys = useSelector(state => state.toysReduce.toys)
+  const dispatch = useDispatch()
+  const location = useLocation()
+  const petId = location.state.pet_id
+  const petName = location.state.name
     // console.log("props from navigate", props.id)
 
-    useEffect(() => {
-        dispatch(fetchToys())
-    }, [])
+  useEffect(() => {
+    dispatch(fetchToys())
+  }, [])
 
-    useEffect(() => {
-      const pet = location.state
-    }, [location])
-    console.log(petId)
-    console.log(petName)
+  useEffect(() => {
+    const pet = location.state
+  }, [location])
+  console.log(petId)
+  console.log(petName)
 
+  function renderToys(petId, toys){
+    // {/* maybe need to filter instead of map? */}
+    if (petId == toys.pet_id){
+      return toys.map(t => <ToyCard key={t.id} {...t}/>)
+    } else {
+      return <h4>There are no toys currently in this toy box.</h4>
+    }
+  }
+
+  const currentPetToys = toys.filter(t => t.id == petId)
+  
   return (
     <div className='toys-list'>
       <div className='toy-box-message'>
@@ -30,7 +41,7 @@ export default function ToysList() {
       </div>
     {console.log(toys)}
     {<ToyForm />}
-    {toys.map(t => <ToyCard key={t.id} {...t}/>)}
+    {currentPetToys.map(t => <ToyCard key={t.id} {...t}/>)}
   </div>
   )
 }
