7e935300a2ae7bc4a9bb058e2ed5aa27a2100a99
dotnotation <dotinmotionproductions@gmail.com>
2022-03-09 21:55:00 -0500

    added logo image and logo text to navbar

--- furmiliar_react_frontend/src/Components/Navbar/Navbar.js
+++ furmiliar_react_frontend/src/Components/Navbar/Navbar.js
@@ -1,13 +1,28 @@
+import { requirePropFactory } from '@mui/material'
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap' 
-import 'bootstrap/dist/css/bootstrap.css'
 
 export default function NavigationBar() {
   return (
     <div>  
-        <Navbar className='nav-bar' bg="light" expand="lg" >
+        <Navbar className='nav-bar' bg="light" expand="xl" >
             <Container fluid>
-                <Navbar.Brand href="/">Furmiliar</Navbar.Brand>
+                <Navbar.Brand href="/">        
+                  <img
+                    alt="Dog with paw logo"
+                    src={require("../../Assets/Logo-image.png")}
+                    height="70"
+                    className="d-inline-block align-top"
+                  />
+                </Navbar.Brand>
+                <Navbar.Brand href="/">        
+                  <img
+                    alt="Dog with paw logo"
+                    src={require("../../Assets/Logo-text.png")}
+                    height="50"
+                    className="d-inline-block align-center"
+                  />
+                </Navbar.Brand>
                 {/* <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}> */}
                 <Nav className="me-auto mb-2 mb-lg-0">
                 <Nav.Link className="active fs-4" href="/toys">Toys</Nav.Link>
