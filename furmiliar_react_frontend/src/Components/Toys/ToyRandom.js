40580d6b59812525db4d07f8f9bcf492a43ac90e
dotnotation <dotinmotionproductions@gmail.com>
2022-03-22 17:08:20 -0400

    added className to span for better styling

--- furmiliar_react_frontend/src/Components/Toys/ToyRandom.js
+++ furmiliar_react_frontend/src/Components/Toys/ToyRandom.js
@@ -18,7 +18,7 @@ export default function ToyRandom() {
         <div className='random-toy'>
             <h2>Toy of the day:</h2>
             <h4 className='mx-auto px-4'>The toy of the day is a randomly selected toy that is one of the highest rated toys on our app. This will hopefully give you inspiration for your pet's next favorite toy.</h4>
-            {toyOfTheDay ? <ToyRandomCard {...toyOfTheDay} /> : <span>It seems like there are no highly rated toys today. Please let us know where your dog's favorite toys is.</span>}
+            {toyOfTheDay ? <ToyRandomCard {...toyOfTheDay} /> : <span className='mx-auto px-4'>It seems like there are no highly rated toys today. Please let us know where your dog's favorite toys is.</span>}
             <div className='buffer'>
                 <br></br>
             </div>
